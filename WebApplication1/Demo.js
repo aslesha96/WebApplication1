97a85574ef5e4cc3e0b489e9dfcdd5c592d84cae
ChA <ChA@HYD002970>
2019-08-08 09:54:19 +0530

    Add project files.

---
+++ WebApplication1/Demo.js
@@ -0,0 +1,26 @@
+﻿var MyApp = angular.module("MyApp", ['ngRoute']);
+
+MyApp.config(['$routeProvider',
+function ($routeProvider) {
+    $routeProvider.
+        when('/RegisterPhy', {
+            templateUrl: 'Views/RegisterPhy.html',
+            controller: 'Phycontroller'
+        }).
+        when('/RegisterPat', {
+            templateUrl: 'Views/RegisterPat.html',
+            controller: 'Patcontroller'
+        }).
+    otherwise({
+        redirectTo: '/'
+    }); 
+}]);
+
+
+MyApp.controller("Phycontroller", function ($scope) {
+    $scope.message = "physician page";
+});
+
+MyApp.controller("Patcontroller", function ($scope) {
+    $scope.message = "patients  page";
+});
